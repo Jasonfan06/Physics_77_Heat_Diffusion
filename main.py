b5940b6e73f512c9f7bace53c54679bf40e180ec
Jason <128120655+Jasonfan06@users.noreply.github.com>
2025-10-31 14:34:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import numpy as np
-from scipy import linalg, fft
+from numpy import linalg as LA
+from scipy import linalg, fft, optimize, signal, stats, integrate
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sp
